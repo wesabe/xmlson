863281d485c6391b429bfbcdcbb6fcc33af79470
Coda Hale <coda@wesabe.com>
2009-03-14 19:53:27 -0700

    Added XmlsonObject.

---
+++ src/main/java/com/wesabe/xmlson/XmlsonObject.java
@@ -0,0 +1,115 @@
+package com.wesabe.xmlson;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * An XMLSON object, which consists of primitive/element pairs and members.
+ * 
+ * @author coda
+ * 
+ */
+public class XmlsonObject extends XmlsonMember {
+	private final Map<XmlsonString, XmlsonElement> properties = new LinkedHashMap<XmlsonString, XmlsonElement>();
+
+	/**
+	 * Creates a named XMLSON object.
+	 * 
+	 * @param name
+	 *            the object's name
+	 */
+	public XmlsonObject(String name) {
+		super(name);
+	}
+
+	/**
+	 * Returns an unmodifiable view of this object's properties.
+	 * 
+	 * @return this object's properties
+	 */
+	public Map<XmlsonString, XmlsonElement> getProperties() {
+		return Collections.unmodifiableMap(properties);
+	}
+
+	/**
+	 * Adds a string property.
+	 * 
+	 * @param key
+	 *            the property's name
+	 * @param value
+	 *            the property's value
+	 * @return {@code this}, for chaining purposes
+	 */
+	public XmlsonObject addProperty(String key, String value) {
+		if (value == null) {
+			return addNullProperty(key);
+		}
+
+		properties.put(new XmlsonString(key), new XmlsonString(value));
+
+		return this;
+	}
+
+	/**
+	 * Adds a numeric property.
+	 * 
+	 * @param key
+	 *            the property's name
+	 * @param value
+	 *            the property's value
+	 * @return {@code this}, for chaining purposes
+	 */
+	public XmlsonObject addProperty(String key, Number value) {
+		if (value == null) {
+			return addNullProperty(key);
+		}
+
+		properties.put(new XmlsonString(key), new XmlsonNumber(value));
+
+		return this;
+	}
+
+	/**
+	 * Adds a boolean property.
+	 * 
+	 * @param key
+	 *            the property's name
+	 * @param value
+	 *            the property's value
+	 * @return {@code this}, for chaining purposes
+	 */
+	public XmlsonObject addProperty(String key, Boolean value) {
+		if (value == null) {
+			return addNullProperty(key);
+		}
+
+		properties.put(new XmlsonString(key), new XmlsonBoolean(value));
+
+		return this;
+	}
+
+	/**
+	 * Adds a null property.
+	 * 
+	 * @param key
+	 *            the property's name
+	 * @return {@code this}, for chaining purposes
+	 */
+	public XmlsonObject addNullProperty(String key) {
+		properties.put(new XmlsonString(key), new XmlsonNull());
+		return this;
+	}
+
+	/**
+	 * Adds a member.
+	 * 
+	 * @param member
+	 *            the member to add
+	 * @return {@code this}, for chaining purposes
+	 */
+	public XmlsonObject add(XmlsonMember member) {
+		properties.put(new XmlsonString(member.getName()), member);
+		return this;
+	}
+}
---
+++ src/test/java/com/wesabe/xmlson/tests/XmlsonObjectTest.java
@@ -0,0 +1,152 @@
+package com.wesabe.xmlson.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+import com.wesabe.xmlson.XmlsonBoolean;
+import com.wesabe.xmlson.XmlsonNull;
+import com.wesabe.xmlson.XmlsonNumber;
+import com.wesabe.xmlson.XmlsonObject;
+import com.wesabe.xmlson.XmlsonString;
+
+@RunWith(Enclosed.class)
+public class XmlsonObjectTest {
+	public static class A_New_Xmlson_Object {
+		@Test
+		public void itHasAName() throws Exception {
+			XmlsonObject object = new XmlsonObject("blah");
+			assertEquals("blah", object.getName());
+		}
+		
+		@Test
+		public void itHasNoProperties() throws Exception {
+			XmlsonObject object = new XmlsonObject("blah");
+			assertTrue(object.getProperties().isEmpty());
+		}
+	}
+	
+	public static class Adding_A_String_Property {
+		@Test
+		public void itReturnsTheObjectForChaining() throws Exception {
+			XmlsonObject object = new XmlsonObject("blah");
+			assertSame(object, object.addProperty("blah", "blah"));
+		}
+		
+		@Test
+		public void itAddsThePropertyToThePropertiesMap() throws Exception {
+			XmlsonObject object = new XmlsonObject("blah");
+			object.addProperty("blah", "blee");
+			
+			XmlsonString property = (XmlsonString) object.getProperties().get(new XmlsonString("blah"));
+			assertEquals("blee", property.getValue());
+		}
+	}
+	
+	public static class Adding_A_Null_String_Property {
+		@Test
+		public void itAddsThePropertyToThePropertiesMap() throws Exception {
+			XmlsonObject object = new XmlsonObject("blah");
+			String blee = null;
+			object.addProperty("blah", blee);
+			
+			XmlsonNull property = (XmlsonNull) object.getProperties().get(new XmlsonString("blah"));
+			assertNull(property.getValue());
+		}
+	}
+	
+	public static class Adding_A_Number_Property {
+		@Test
+		public void itReturnsTheObjectForChaining() throws Exception {
+			XmlsonObject object = new XmlsonObject("blah");
+			assertSame(object, object.addProperty("blah", 100));
+		}
+		
+		@Test
+		public void itAddsThePropertyToThePropertiesMap() throws Exception {
+			XmlsonObject object = new XmlsonObject("blah");
+			object.addProperty("blah", 100);
+			
+			XmlsonNumber property = (XmlsonNumber) object.getProperties().get(new XmlsonString("blah"));
+			assertEquals(100, property.getValue());
+		}
+	}
+	
+	public static class Adding_A_Null_Number_Property {
+		@Test
+		public void itAddsThePropertyToThePropertiesMap() throws Exception {
+			XmlsonObject object = new XmlsonObject("blah");
+			Number blee = null;
+			object.addProperty("blah", blee);
+			
+			XmlsonNull property = (XmlsonNull) object.getProperties().get(new XmlsonString("blah"));
+			assertNull(property.getValue());
+		}
+	}
+	
+	public static class Adding_A_Boolean_Property {
+		@Test
+		public void itReturnsTheObjectForChaining() throws Exception {
+			XmlsonObject object = new XmlsonObject("blah");
+			assertSame(object, object.addProperty("blah", false));
+		}
+		
+		@Test
+		public void itAddsThePropertyToThePropertiesMap() throws Exception {
+			XmlsonObject object = new XmlsonObject("blah");
+			object.addProperty("blah", false);
+			
+			XmlsonBoolean property = (XmlsonBoolean) object.getProperties().get(new XmlsonString("blah"));
+			assertEquals(false, property.getValue());
+		}
+	}
+	
+	public static class Adding_A_Null_Boolean_Property {
+		@Test
+		public void itAddsThePropertyToThePropertiesMap() throws Exception {
+			XmlsonObject object = new XmlsonObject("blah");
+			Boolean blee = null;
+			object.addProperty("blah", blee);
+			
+			XmlsonNull property = (XmlsonNull) object.getProperties().get(new XmlsonString("blah"));
+			assertNull(property.getValue());
+		}
+	}
+	
+	public static class Adding_A_Null_Property {
+		@Test
+		public void itReturnsTheObjectForChaining() throws Exception {
+			XmlsonObject object = new XmlsonObject("blah");
+			assertSame(object, object.addNullProperty("blah"));
+		}
+		
+		@Test
+		public void itAddsThePropertyToThePropertiesMap() throws Exception {
+			XmlsonObject object = new XmlsonObject("blah");
+			object.addNullProperty("blah");
+			
+			XmlsonNull property = (XmlsonNull) object.getProperties().get(new XmlsonString("blah"));
+			assertNull(property.getValue());
+		}
+	}
+	
+	public static class Adding_A_Member {
+		@Test
+		public void itReturnsTheObjectForChaining() throws Exception {
+			XmlsonObject object = new XmlsonObject("blah");
+			assertSame(object, object.add(new XmlsonObject("blah")));
+		}
+		
+		@Test
+		public void itAddsTheMemberToThePropertyMap() throws Exception {
+			XmlsonObject object = new XmlsonObject("blah");
+			XmlsonObject member = new XmlsonObject("blee");
+			object.add(member);
+			
+			XmlsonObject otherMember = (XmlsonObject) object.getProperties().get(new XmlsonString("blee"));
+			assertSame(member, otherMember);
+		}
+	}
+}
